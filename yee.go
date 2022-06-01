6841a41b30cd04be54e3996d3361af3777f2a9a3
HenryYee <henry@yearning.io>
2022-06-01 16:17:19 +0800

    v0.3.3

--- yee.go
+++ yee.go
@@ -39,7 +39,7 @@ type Core struct {
 	Banner                 bool
 }
 
-const version = "yee v0.3.0"
+const version = "yee v0.3.3"
 
 const creator = "Creator: Henry Yee"
 const title = "-----Easier and Faster-----"
